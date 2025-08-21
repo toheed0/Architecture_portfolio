89ed3b666efa432b2ae8fd079a00d49a9dfca389
Toheed69 <mughaltoheed29@gmail.com>
2025-08-21 16:37:43 +0500

    update project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -24,6 +24,9 @@ import interior2 from "../assets/interior2.jpg";
 import landscape from "../assets/landscape.jpg";
 import landscape1 from "../assets/landscape1.jpg";
 import landscape2 from "../assets/lc.jpg";
+import lc1 from "../assets/lc1.jpg";
+import lc2 from "../assets/lc2.jpg";
+import lc3 from "../assets/lc3.jpg";
 import lobby5 from "../assets/lobby/lobby10.jpg";
 import lobby6 from "../assets/lobby/lobby11.jpg";
 import lobby7 from "../assets/lobby/lobby12.jpg";
@@ -52,37 +55,54 @@ import ar20 from "../assets/architecture/ar20.jpg";
 import ar21 from "../assets/architecture/ar21.jpg";
 import ar22 from "../assets/architecture/ar22.jpg";
 import ar23 from "../assets/architecture/ar23.jpg";
+import arch3 from "../assets/architecture/arch3.jpg";
+import arch4 from "../assets/architecture/arch4.jpg";
+import arch5 from "../assets/architecture/arch5.jpg";
+import arch6 from "../assets/architecture/arch6.jpg";
+import arch7 from "../assets/architecture/arch7.jpg";
+import arch8 from "../assets/architecture/arch8.jpg";
+import arch9 from "../assets/architecture/arch9.jpg";
+import arch10 from "../assets/architecture/arch10.jpg";
+import arch11 from "../assets/architecture/arch11.jpg";
+import arch12 from "../assets/architecture/arch12.jpg";
 import int21 from "../assets/interior/interior21.jpg"
 import int22 from "../assets/interior/interior22.jpg";
 import int23 from "../assets/interior/interior23.jpg";
 import int24 from "../assets/interior/interior24.jpg";
 import arr1 from "../assets/arr1.jpg";
+import int4 from "../assets/interior/int4.jpg";
 import lo from "../assets/lo.jpg";
+import lo11 from "../assets/lobby/lo11.jpg";
+import lo12 from "../assets/lobby/lo12.jpg";
+import lo13 from "../assets/lobby/lo13.jpg";
+import lo14 from "../assets/lobby/lo14.jpg";
+import lo15 from "../assets/lobby/lo15.jpg";
+import lo16 from "../assets/lobby/lo16.jpg";
 const allProjects = [
   { id: 1,  title: "Luxury Villa",        img: architecture3, category: "Elevation" },
   { id: 2,  title: "Luxury Interior",     img: project2,      category: "Interior" },
   { id: 3,  title: "Landscape Garden",    img: landscape,     category: "Landscape" },
   { id: 4,  title: "Luxury Bedroom",      img: bedroom,       category: "Bedrooms" },
   { id: 5,  title: "Cozy Bedroom",        img: bedroom1,      category: "Bedrooms" },
-  { id: 6,  title: "Lobby",               img: lobby,         category: "Lobby" },
+  { id: 6,  title: "Interior",               img: lobby,         category: "Interior" },
   { id: 7,  title: "Premium Kitchen",     img: interior,      category: "Interior" },
   { id: 8,  title: "Kitchen",             img: interior1,     category: "Interior" },
   { id: 9,  title: "City Lobby",          img: lobby1,        category: "Lobby" },
   { id:10,  title: "Villa",               img: architecture2, category: "Elevation" },
   { id:11,  title: "Modern Villa",        img: architecture4, category: "Elevation" },
-  { id:12,  title: "Luxury Interior",     img: interior2,     category: "Interior" },
+  { id:12,  title: "Luxury Bedroom",     img: interior2,     category: "Bedrooms" },
   { id:13,  title: "Black Theme",         img: bedroom2,      category: "Bedrooms" },
   { id:14,  title: "Garden",              img: landscape1,    category: "Landscape" },
   { id:15,  title: "Villa",               img: architecture1, category: "Elevation" },
   { id:16,  title: "Villa",               img: architecture5, category: "Elevation" },
   { id:17,  title: "Villa",               img: architecture6, category: "Elevation" },
   { id:18,  title: "Lobby Premium",       img: lobby2,        category: "Lobby" },
-  { id:19,  title: "Luxury Lobby",        img: lobby3,        category: "Lobby" },
+  { id:19,  title: "Luxury Interior",        img: lobby3,        category: "Interior" },
   { id:20,  title: "Modern Villa",        img: architecture7, category: "Elevation" },
   { id:21,  title: "Vill Garden",              img: landscape2,    category: "Landscape" },
-  { id:22,  title: "Luxury Lobby",        img: lobby5,        category: "Lobby" },
+  { id:22,  title: "Luxury ",        img: lobby5,        category: "Interior" },
   { id:23,  title: "Luxury Lobby",        img: lobby6,        category: "Lobby" },
-  { id:24,  title: "Luxury Lobby",        img: lobby7,        category: "Lobby" },
+  { id:24,  title: "Luxury Interior",        img: lobby7,        category: "Interior" },
   { id: 25,  title: "Cozy Bedroom",        img: bedroom5,      category: "Bedrooms" },
   { id: 26,  title: " Bedroom",        img:bedroom6 ,      category: "Bedrooms" },
   { id: 27,  title: "Luxury Bedroom",        img: bedroom7,      category: "Bedrooms" },
@@ -113,7 +133,26 @@ const allProjects = [
   { id:54,  title: "Luxury Interior",        img: int23, category: "Interior" },
   { id:55,  title: "Premium Interior",       img: int24, category: "Interior" },
   { id:56,  title: "Modern Villa",        img: arr1, category: "Elevation" },
-  { id: 57,  title: "Lobby",               img: lo,         category: "Lobby" },
+  { id:57,  title: "Lobby",               img: lo,         category: "Lobby" },
+  { id:58,  title: "Modern Villa",        img: arch3, category: "Elevation" },
+  { id:59,  title: "Modern Villa",        img: arch4, category: "Elevation" },
+  { id:60,  title: "Modern Villa",        img: arch5, category: "Elevation" },
+  { id:61,  title: "Modern Villa",        img: arch6, category: "Elevation" },
+  { id:62,  title: "Modern Villa",        img: arch7, category: "Elevation" },
+  { id:63,  title: "Modern Villa",        img: arch8, category: "Elevation" },
+  { id:64,  title: "Modern Villa",        img: arch9, category: "Elevation" },
+  { id:65,  title: "Modern Villa",        img: arch10, category: "Elevation" },
+  { id:66,  title: "Modern Villa",        img: arch11, category: "Elevation" },
+  { id:67,  title: "Interior",        img: int4, category: "Interior" },
+  { id:68,  title: "Luxury Lobby",        img: lo11,        category: "Lobby" },
+  { id:69,  title: "Luxury Lobby",        img: lo12,        category: "Lobby" },
+  { id:70,  title: "Luxury Lobby",        img: lo13,        category: "Lobby" },
+  { id:71,  title: "Luxury Lobby",        img: lo14,        category: "Lobby" },
+  { id:72,  title: "Luxury Lobby",        img: lo15,        category: "Lobby" },
+  { id:73,  title: "Luxury Lobby",        img: lo16,        category: "Lobby" },
+  { id:74,  title: "Landscap",              img: lc1,    category: "Landscape" },
+  { id:14,  title: "Luxury Pool",              img: lc2,    category: "Landscape" },
+  { id:14,  title: "Premium pool",              img: lc3,    category: "Landscape" },
 ];
 
 
